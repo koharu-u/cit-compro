090bab98b1b010798236de4048708e73d84015d0
koharu <koharudotgit@duck.com>
2026-07-09 21:59:36 +0700

    feat: implement for week2 (1-3)

---
+++ week2/2-variable.py
@@ -0,0 +1,12 @@
+x = 0
+x += 2
+# Expected: 2
+print(x)
+
+x *= 3
+# Expected: 6
+print(x)
+
+x = -x
+# Expected: -6
+print(x)
---
+++ week2/3-kncalc.py
@@ -0,0 +1,3 @@
+mass = float(input("m = "))
+velocity = float(input("v = "))
+print(f"K.E. = {((1/2)*mass*(velocity**2)):.2f}")
