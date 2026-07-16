e3cf43bad624c8a1f8de37c41024fd5230a660a6
koharu <koharudotgit@duck.com>
2026-07-16 16:07:01 +0700

    fix(1-5-price): fix if condition

--- week3/1-5-price.py
+++ week3/1-5-price.py
@@ -3,13 +3,13 @@ tot_p = item1+item2+item3+item4
 print("Total Price =", tot_p)
 
 # using if conditions for finding lowest price
-if(item1 < item2 and item1 < item3 and item1 < item4):
+if(item1 <= item2 and item1 <= item3 and item1 <= item4):
     print("Money to pay =", tot_p - item1)
-elif(item2 < item1 and item2 < item1 and item2 < item3):
+elif(item2 <= item1 and item2 <= item1 and item2 <= item3):
     print("Money to pay =", tot_p - item2)
-elif(item3 < item1 and item3 < item2 and item3 < item4):
+elif(item3 <= item1 and item3 <= item2 and item3 <= item4):
     print("Money to pay =", tot_p - item3)
-elif(item4 < item1 and item4 < item2 and item4 < item3):
+elif(item4 <= item1 and item4 <= item2 and item4 <= item3):
     print("Money to pay =", tot_p - item4)
 
 # using min() function for finding lowest price
